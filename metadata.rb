25e2ecf430b046b1779f2923c9c2721bce709161
Ryan Walker <ryan.walker@rackspace.com>
2013-01-30 10:30:22 -0600

    Remove git cookbook - just install the package

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,6 @@ description      "Installs/Configures etherpad-lite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1.0"
 
-depends "git"
 depends "build-essential"
 depends "database"
 depends "mysql"
